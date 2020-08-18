b3cbe54b6f122d4bd7ca3073bedb864b3940a6ad
mlangTse <48012544+mlangTse@users.noreply.github.com>
2020-08-18 17:32:14 +1000

    done

--- exam/part2Q1/src/part2Q1/SalesPerson.java
+++ exam/part2Q1/src/part2Q1/SalesPerson.java
@@ -5,14 +5,15 @@ public class SalesPerson extends Employee {
 	private float commission;
 	private double salesAchieved;
 
-	public SalesPerson(String title, String firstName, String lastName, int quota) {
+	public SalesPerson(String title, String firstName, String lastName, int quota, double salesAchieved) {
 		super(title, firstName, lastName, quota);
+		this.salesAchieved = salesAchieved;
 	}
 	// this is duplicated code, to avoid this, we create a abstract  method in their super class
     @Override
 	public double calculateSalary() {
 		double totalSal;
-		totalSal = super.getBaseSalary() + commission * super.getSalesAchieved()
+		totalSal = super.getBaseSalary() + commission * getSalesAchieved()
 		         + super.calculateParkingFringeBenefits()
 		         - super.calculateTax();
 		return totalSal;
